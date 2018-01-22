08a9d6b0c6a4af4bbf58ed7bf557d271a1d534fa
ljliu <lijun.liu@sail-fs.com>
2018-01-22 16:03:06 +0800

    update

--- src/abs/ProductDetail.tsx
+++ src/abs/ProductDetail.tsx
@@ -1,18 +1,25 @@
 import * as React from 'react';
 import { NavBar, Icon } from 'antd-mobile'; 
- 
+import { Link } from 'react-router-dom';
+
 interface Props {
     title: string;
 }
   
 export default class MyComponent extends React.Component<Props, {}> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+          lastPath: props.title,
+        }; 
+    } 
+
     render() {  
         return (
             <div>
                 <NavBar
                     mode="light"
-                    icon={<Icon type="left" />}
-                    onLeftClick={() => console.log('onLeftClick')}
+                    icon={<Link to="/product"><Icon type="left" /></Link>} 
                     rightContent={[
                         <Icon key="0" type="search" style={{ marginRight: '16px' }} />,
                         <Icon key="1" type="ellipsis" />,
