e0c7a270cfe187f8f6a94c3aaa8e15b65e9952fb
ljliu <lijun.liu@sail-fs.com>
2018-01-23 16:05:02 +0800

    UPDATE

--- src/abs/Market.tsx
+++ src/abs/Market.tsx
@@ -9,38 +9,13 @@ interface Props {
     title: string;
 }
   
-export default class MarketComponent extends React.Component<Props, any> {
-    timerID: any; 
-    constructor(props: Props) {
-        super(props); 
-        this.state = {date: new Date()};
-    }
-    componentDidMount() {
-        this.timerID = this.updateTimer();
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
-
-    updateTimer() {
-        return setInterval(
-            () => {
-                this.setState({
-                    date: new Date()
-                });
-            },
-            1000
-          );
-    } 
-
+export default class MarketComponent extends React.Component<Props, any> { 
     render() { 
         return (
             <div>
                 <ABSPanel title="市场概要" >
                     <ABSTableMarket />   
-                </ABSPanel>
-                <div><span>{this.state.date.toLocaleTimeString()}.</span></div>
+                </ABSPanel> 
                 <ABSPanel title="发行统计">
                     <ABSChartMarket />
                 </ABSPanel>
