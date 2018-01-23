aae526ba81d2212db8da599b43def407f3442281
邓大嘴 <616287091@qq.com>
2018-01-23 10:47:32 +0800

    add component

--- src/abs/Market.tsx
+++ src/abs/Market.tsx
@@ -1,64 +1,17 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-import './abs-panel/abs-panel.css';
+import './abs-panel/abs-panel.less';
 import 'amfe-flexible';
+import ABSPanelComponent from './abs-panel/abs-panel';
 
 interface Props {
     title: string;
 }
   
-export default class MyComponent extends React.Component<Props, {}> {
+export default class MarketComponent extends React.Component<Props, {}> {
     render() { 
         return (
-            <div className="abs-panel-body">
-                <div id="root" className="abs-panel-content"> 
-                    <div className="abs-panel-panel  abs-panel-panel_mb" >
-                        <div className="abs-panel-title"><span>市场概要</span></div>
-                        <div className="abs-panel-container">
-                            <table className="abs-panel-table">
-                                <thead>
-                                    <tr>
-                                        <th>分类</th>
-                                        <th className="text-right">今年(单)</th>
-                                        <th className="text-right">今年(亿)</th>
-                                        <th className="text-right">累计(亿)</th>
-                                    </tr> 
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td><Link to="/productDetail/10">企业ABN</Link></td>
-                                        <td>10</td>
-                                        <td>110</td>
-                                        <td>110</td>
-                                    </tr>
-                                    <tr>
-                                        <td><Link to="/productDetail/10">企业ABN</Link></td>
-                                        <td>10</td>
-                                        <td>110</td>
-                                        <td>110</td>
-                                    </tr>
-                                    <tr>
-                                        <td><Link to="/productDetail/10">企业ABN</Link></td>
-                                        <td>10</td>
-                                        <td>110</td>
-                                        <td>110</td>
-                                    </tr>
-                                    <tr>
-                                        <td><Link to="/productDetail/10">企业ABN</Link></td>
-                                        <td>10</td>
-                                        <td>110</td>
-                                        <td>110</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                    <div className="abs-panel-panel">
-                        <div className="abs-panel-title"><span>发行统计</span></div>
-                        <div>highcharts</div>
-                    </div> 
-                </div> 
-            </div>
+            <abs-panel title='sss'></abs-panel>
         );
     }
 }
---
+++ src/abs/abs-panel/abs-panel.tsx
@@ -0,0 +1,71 @@
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import './abs-panel/abs-panel.less';
+import 'amfe-flexible';
+
+interface Props {
+    title: string;
+}
+
+export default class ABSPanelComponent extends React.Component<Props, {}> {
+    render() {
+        return (
+            <div className="abs-panel-body">
+                <div id="root" className="abs-panel-content">
+                    <div className="abs-panel-panel  abs-panel-panel_mb" >
+                        <div className="abs-panel-title"><span>{this.props.title}</span></div>
+                        <div className="abs-panel-container">
+                            <table className="abs-panel-table">
+                                <thead>
+                                    <tr>
+                                        <th>分类</th>
+                                        <th className="text-right">今年(单)</th>
+                                        <th className="text-right">今年(亿)</th>
+                                        <th className="text-right">累计(亿)</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td><Link to="/productDetail/10">企业ABN</Link></td>
+                                        <td>10</td>
+                                        <td>110</td>
+                                        <td>110</td>
+                                    </tr>
+                                    <tr>
+                                        <td><Link to="/productDetail/10">企业ABN</Link></td>
+                                        <td>10</td>
+                                        <td>110</td>
+                                        <td>110</td>
+                                    </tr>
+                                    <tr>
+                                        <td><Link to="/productDetail/10">企业ABN</Link></td>
+                                        <td>10</td>
+                                        <td>110</td>
+                                        <td>110</td>
+                                    </tr>
+                                    <tr>
+                                        <td><Link to="/productDetail/10">企业ABN</Link></td>
+                                        <td>10</td>
+                                        <td>110</td>
+                                        <td>110</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div className="abs-panel-panel">
+                        <div className="abs-panel-title"><span>发行统计</span></div>
+                        <div>highcharts</div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+ReactDOM.render(
+    <abs-panel title="市场概要" />,
+    mountNode
+);
