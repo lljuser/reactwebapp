d7187873070c528367d140a0fd665b95b854d7fa
ljliu <lijun.liu@sail-fs.com>
2018-01-27 12:44:48 +0800

    update

--- src/abs/services/market/index.ts
+++ src/abs/services/market/index.ts
@@ -1,5 +1,5 @@
 import { MarketApi } from '../../config/api';
-import Request from '../../components/http/request';
+import Request from  '../../../components/http/request';
 
 class MarketService {
 
