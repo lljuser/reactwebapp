c10bab4d8fca5378bba4605efcaa492da2200a17
ljliu <lijun.liu@sail-fs.com>
2018-01-29 13:41:34 +0800

    update

--- src/components/ayncLoader.ts
+++
@@ -1,9 +0,0 @@
-import Loadable from 'react-loadable';
-import Loading from '../components/loading';
-
-export default function asyncLoader(module: Promise<any>) {
-  return Loadable({
-    loader: () => module,
-    loading: Loading
-  });
-}
