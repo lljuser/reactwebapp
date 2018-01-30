0ccedb6b51aa24b8fc5d7f79f47f28a5214645d4
zphe <zhipeng.he@sail-fs.com>
2018-01-30 13:24:02 +0800

    暂存修改

---
+++ src/abs/services/trade/index.ts
@@ -0,0 +1,99 @@
+import { TradeApi } from '../../config/api';
+import Request from '../../../common/http/request/index';
+
+class TradeService {
+    /**
+     * 调用获取期限集合接口
+     * 
+     * @memberof Trade
+     */
+    async getWalbuckList() {
+        let returnWalbuckList: any = {
+            walbuckList: [],
+            walbuckValues: []
+        };
+        let res: any = await Request.post(TradeApi.walbuckList, {});
+        if (res.length > 0) {
+            res.forEach(element => {
+                returnWalbuckList.walbuckList.push({ label: element.Value, value: element.Key });
+            });
+            returnWalbuckList.walbuckValues.push(returnWalbuckList.walbuckList[0].value);
+        }
+        return returnWalbuckList;
+    }
+
+    async getCouponList() {
+        let returnCouponList: any = {
+            couponList: [],
+            couponValues: []
+        };
+        let res: any = await Request.post(TradeApi.couponList, {});
+        if (res.length > 0) {
+            res.forEach(element => {
+                returnCouponList.couponList.push({ label: element.Value, value: element.Key });
+            });
+            returnCouponList.couponValues.push(returnCouponList.couponList[0].value);
+        }
+        return returnCouponList;
+    }
+
+    /**
+     * 调用获取评级集合接口
+     * 
+     * @memberof Trade
+     */
+    async  getRatingList() {
+        let returnRatingList: any = {
+            ratingList: [],
+            ratingValues: []
+        };
+        let res: any = await Request.post(TradeApi.ratingList, {});
+        if (res.length > 0) {
+            res.forEach(element => {
+                returnRatingList.ratingList.push({ label: element.Value, value: element.Key });
+            });
+            returnRatingList.ratingValues.push(returnRatingList.ratingList[0].value);
+        }
+        return returnRatingList;
+    }
+
+    async genData(cmd: string, refresh: boolean, direction: number, pageIndex: number, rData: any[], ratingValues: any[], couponValues: any[], walbuckValues: any[], rows: number) {
+        let returnGenData: any = {
+            rData: [],
+            info: '',
+            hasMore: true,
+        };
+        if (refresh) {
+            pageIndex = 1;
+            rData = [];
+        }
+
+        var ratingValue = (ratingValues[0] === undefined ? 0 : ratingValues[0]) as string;
+        var couponValue = (couponValues[0] === undefined ? 0 : couponValues[0]) as string;
+        var walbuckValue = (walbuckValues[0] === undefined ? 0 : walbuckValues[0]) as string;
+
+        var url = TradeApi.list;
+        url = url + '/' + ratingValue + '/' + couponValue + '/' + walbuckValue;
+        url = url + '/' + direction + '/' + pageIndex * rows + '/' + rows;
+
+        let res = await Request.post(url, {});
+        if (res.length === 0) {
+            if (cmd === 'onRefresh') {
+                rData = [];
+                returnGenData.rData = rData;
+                returnGenData.info = '没有更多了';
+                returnGenData.hasMore = false;
+            } else {
+                returnGenData.info = '没有更多了';
+                returnGenData.hasMore = false;
+            }
+
+        } else {
+            rData = [...rData, ...res];
+            returnGenData.rData = rData;
+            returnGenData.info = '加载完成';
+        }
+        return returnGenData;
+    }
+}
+export default new TradeService();
