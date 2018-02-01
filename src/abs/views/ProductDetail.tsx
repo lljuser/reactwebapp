d698c9df3924924fd1952c75a5907a062dfb87c2
ljliu <lijun.liu@sail-fs.com>
2018-02-02 00:31:33 +0800

    update

--- src/abs/views/ProductDetail.tsx
+++ src/abs/views/ProductDetail.tsx
@@ -10,6 +10,8 @@ import ABSChartMarket from '../components/abs-chart';
 import { connect } from 'dva';
 import '../../public/css/themeCopy.less';
 import RoutePageList from '../RouterConfig';
+import ContentLoader, { Facebook } from 'react-content-loader';
+import SpinnerLoader from 'react-loader-spinner';
 
 function mapStateToProps(state: any) {
   return {
@@ -39,6 +41,20 @@ class ProductDetail extends React.Component<any, any> {
                 } 
             }}  
           />
+          <SpinnerLoader 
+            type="Grid"
+            color="orange"
+            height="100"	
+            width="100"
+          /> 
+          <SpinnerLoader type="Oval" color="#00BFFF" height={80} width={80}/>  
+          <Facebook speed={2} primaryColor="#ffc446" secondaryColor="#444444" />
+          <ContentLoader speed={10} primaryColor="#ffc446" secondaryColor="#444444">
+            {/* Pure SVG */}
+            <rect x="0" y="0" rx="5" ry="5" width="70" height="70" />
+            <rect x="80" y="17" rx="4" ry="4" width="300" height="13" />
+            <rect x="80" y="40" rx="3" ry="3" width="250" height="10" />
+          </ContentLoader>
         </div>
         <div className="appH5_body">
         <div className="appH5_content">
