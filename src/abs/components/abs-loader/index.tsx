d54dd630081a409384c28858caf7484b9191ad34
邓大嘴 <616287091@qq.com>
2018-02-02 12:06:00 +0800

    update loading module

---
+++ src/abs/components/abs-loader/index.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import ContentLoader, { Facebook } from 'react-content-loader';
+import SpinnerLoader from 'react-loader-spinner';
+import './index.less';
+import 'amfe-flexible';
+
+interface Props {
+    title?: string;
+    data?: any;
+    minHeight?: string;
+    className?: string;
+}
+// 动画-圆圈型
+export class ABSSpinnerLoader extends React.Component<Props, {}> {
+
+    constructor(props: any) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="abs-loader abs-loader-spinner">
+                <SpinnerLoader
+                    type="Grid"
+                    color="orange"
+                    height="100"
+                    width="100"
+                />
+                <SpinnerLoader type="Oval" color="#00BFFF" height={80} width={80} />
+                <Facebook speed={2} primaryColor="#ffc446" secondaryColor="#444444" />
+                
+            </div>
+        );
+    }
+}
+// 动画-内容型
+export class ABSContentLoader extends React.Component<Props, {}> {
+
+    constructor(props: any) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="abs-loader abs-loader-content">
+                <ContentLoader speed={10} primaryColor="#ffc446" secondaryColor="#444444">
+                    {/* Pure SVG */}
+                    <rect x="0" y="0" rx="5" ry="5" width="70" height="70" />
+                    <rect x="80" y="17" rx="4" ry="4" width="300" height="13" />
+                    <rect x="80" y="40" rx="3" ry="3" width="250" height="10" />
+                </ContentLoader>
+            </div>
+        );
+    }
+} 
--- src/abs/views/ProductDetail.tsx
+++ src/abs/views/ProductDetail.tsx
@@ -7,11 +7,10 @@ import Structure from './product/Structure';
 import NoteList from './product/NoteList';
 import { ABSNavBar } from '../components/abs-navbar';
 import ABSChartMarket from '../components/abs-chart';
+import { ABSSpinnerLoader, ABSContentLoader } from '../components/abs-loader';
 import { connect } from 'dva';
 import '../../public/css/themeCopy.less';
 import RoutePageList from '../RouterConfig';
-import ContentLoader, { Facebook } from 'react-content-loader';
-import SpinnerLoader from 'react-loader-spinner';
 
 function mapStateToProps(state: any) {
   return {
@@ -41,41 +40,29 @@ class ProductDetail extends React.Component<any, any> {
                 } 
             }}  
           />
-          <SpinnerLoader 
-            type="Grid"
-            color="orange"
-            height="100"	
-            width="100"
-          /> 
-          <SpinnerLoader type="Oval" color="#00BFFF" height={80} width={80}/>  
-          <Facebook speed={2} primaryColor="#ffc446" secondaryColor="#444444" />
-          <ContentLoader speed={10} primaryColor="#ffc446" secondaryColor="#444444">
-            {/* Pure SVG */}
-            <rect x="0" y="0" rx="5" ry="5" width="70" height="70" />
-            <rect x="80" y="17" rx="4" ry="4" width="300" height="13" />
-            <rect x="80" y="40" rx="3" ry="3" width="250" height="10" />
-          </ContentLoader>
+          <ABSSpinnerLoader>加载动画-圆圈</ABSSpinnerLoader>
+          <ABSContentLoader>加载动画-内容</ABSContentLoader>
         </div>
         <div className="appH5_body">
-        <div className="appH5_content">
-          <React.Fragment>
-            <ABSPanel title="产品要素" >
-              <Detail detail={this.props.detail} />
-            </ABSPanel>
-            <ABSPanel title="证券结构" >
-              <Structure noteConsTable={this.props.noteConsTable} />
-            </ABSPanel>
-            <ABSPanel title="证券列表" >
-              <NoteList detail={this.props.detail} />
-            </ABSPanel>
-            {
-              (!this.props.chart) ? null :
-                <ABSPanel title="证券偿付">
-                    <ABSChartMarket data={this.props.chart} />
-                </ABSPanel>
-            }
-            
-          </React.Fragment>
+          <div className="appH5_content">
+            <React.Fragment>
+              <ABSPanel title="产品要素" >
+                <Detail detail={this.props.detail} />
+              </ABSPanel>
+              <ABSPanel title="证券结构" >
+                <Structure noteConsTable={this.props.noteConsTable} />
+              </ABSPanel>
+              <ABSPanel title="证券列表" >
+                <NoteList detail={this.props.detail} />
+              </ABSPanel>
+              {
+                (!this.props.chart) ? null :
+                  <ABSPanel title="证券偿付">
+                      <ABSChartMarket data={this.props.chart} />
+                  </ABSPanel>
+              }
+              
+            </React.Fragment>
           </div>
         </div>
       </div>
