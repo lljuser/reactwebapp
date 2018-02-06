5d30c303e9e7bf6f5b2db41e8dc44385fc4967dc
zphe <zhipeng.he@sail-fs.com>
2018-02-06 11:59:21 +0800

    封装pickerchildren组件

---
+++ src/abs/views/components/abs-pickerchildren/index.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/fontawesome-free-solid';
+
+// 如果不是使用 List.Item 作为 children
+interface Props {
+    onClick?: any;
+    extra?: any;
+    first?: string;
+}
+
+export default class PickerChildren extends React.Component<Props, {}> {
+    constructor(props: any) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div onClick={this.props.onClick} className={this.props.first ? 'picker-trigger first' : 'picker-trigger'}>
+                <div className="selector">{this.props.extra}
+                    <FontAwesomeIcon icon={faChevronDown} />
+                    {/* <i className="iconfont arrow-down">&#xe692;</i> */}
+                </div>
+            </div>
+        );
+    }
+}  
