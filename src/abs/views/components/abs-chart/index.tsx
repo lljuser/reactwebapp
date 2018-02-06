380d7a4f11a11a3977c91b221072f2c530fc55af
ljliu <lijun.liu@sail-fs.com>
2018-02-06 15:42:56 +0800

    update

--- src/abs/views/components/abs-chart/index.tsx
+++ src/abs/views/components/abs-chart/index.tsx
@@ -2,8 +2,7 @@ import * as React from 'react';
 import ReactHighcharts from 'react-highcharts';
 import '../index.less'; 
 import ChartTheme from '../../../config/chartTheme';
-
-console.log(ChartTheme);
+ 
 ReactHighcharts.Highcharts.setOptions(ChartTheme);
 
 interface Props {
