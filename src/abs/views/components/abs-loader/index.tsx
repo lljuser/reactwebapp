bcb697d8aa07778c4a9e2031cc66fc4b9d7fe815
ljliu <lijun.liu@sail-fs.com>
2018-02-07 14:29:21 +0800

    update

--- src/abs/views/components/abs-loader/index.tsx
+++ src/abs/views/components/abs-loader/index.tsx
@@ -1,51 +1,45 @@
 import * as React from 'react';
-import ContentLoader, { Facebook } from 'react-content-loader';
-import SpinnerLoader from 'react-loader-spinner';
+import { Facebook, BulletList } from 'react-content-loader';
 import './index.less';
 import 'amfe-flexible';
-
-interface Props {
-    title?: string;
-    data?: any;
-    minHeight?: string;
-    className?: string;
-}
-// 动画-圆圈型
-export class ABSSpinnerLoader extends React.Component<Props, {}> {
-
-    constructor(props: any) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div className="abs-loader abs-loader-spinner"> 
-                <SpinnerLoader type="Oval" color="#00BFFF" height={80} width={80} /> 
-            </div>
-        );
-    }
-}
+import ABSPanelComponent from '../abs-panel/index'; 
+ 
 // 动画-内容型
-export class ABSContentLoader extends React.Component<Props, {}> {
+export class AbsContentLoader extends React.Component<any, {}> {
 
     constructor(props: any) {
         super(props);
     }
-
     render() {
+        // const y = 70;
         return (
-            <div className="abs-loader abs-loader-content">
-                <Facebook speed={2} primaryColor="#535353" secondaryColor="gray" />
-                <Facebook speed={2} primaryColor="#535353" secondaryColor="gray" />
-                <Facebook speed={2} primaryColor="#535353" secondaryColor="gray" />
-                <Facebook speed={2} primaryColor="#535353" secondaryColor="gray" />
-                <ContentLoader speed={2} primaryColor="#535353" secondaryColor="gray">
-                    {/* Pure SVG */}
-                    <rect x="0" y="0" rx="5" ry="5" width="70" height="70" />
-                    <rect x="80" y="17" rx="4" ry="4" width="300" height="13" />
-                    <rect x="80" y="40" rx="3" ry="3" width="250" height="10" />
-                </ContentLoader>
-            </div>
+            <ABSPanelComponent>
+                <Facebook   speed={2} primaryColor={'#535353'}  secondaryColor={'gray'} />
+                <Facebook   speed={2} primaryColor={'#535353'}  secondaryColor={'gray'} />
+                <Facebook   speed={2} primaryColor={'#535353'}  secondaryColor={'gray'} />
+                <Facebook   speed={2} primaryColor={'#535353'}  secondaryColor={'gray'} />
+                <Facebook   speed={2} primaryColor={'#535353'}  secondaryColor={'gray'} />
+                <Facebook   speed={2} primaryColor={'#535353'}  secondaryColor={'gray'} /> 
+            </ABSPanelComponent> 
         );
     }
 } 
+
+export class AbsListLoader extends React.Component<any, {}> { 
+    constructor(props: any) {
+        super(props);
+    }
+    render() {
+        // const y = 70;
+        return (
+            <ABSPanelComponent>
+                <BulletList    speed={2} primaryColor={'#535353'}  secondaryColor={'gray'} />
+                <BulletList    speed={2} primaryColor={'#535353'}  secondaryColor={'gray'} />
+                <BulletList    speed={2} primaryColor={'#535353'}  secondaryColor={'gray'} />
+                <BulletList    speed={2} primaryColor={'#535353'}  secondaryColor={'gray'} />
+                <BulletList    speed={2} primaryColor={'#535353'}  secondaryColor={'gray'} />
+                <BulletList    speed={2} primaryColor={'#535353'}  secondaryColor={'gray'} />  
+            </ABSPanelComponent> 
+        );
+    }
+}  
