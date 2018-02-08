436360f57f217d0f366911ef44ef7e5400a2ec9c
ljliu <lijun.liu@sail-fs.com>
2018-02-08 14:52:38 +0800

    update

--- src/common/components/react-loader-spinner/src/loading/grid.js
+++ src/common/components/react-loader-spinner/src/loading/grid.js
@@ -8,50 +8,50 @@ return (
          values="1;.2;1" calcMode="linear"
          repeatCount="indefinite" />
     </circle>
-    <circle cx="12.5" cy="52.5" r="12.5" fill-opacity=".5">
-        <animate attributeName="fill-opacity"
+    <circle cx="12.5" cy="52.5" r="12.5" fillOpacity=".5">
+        <animate attributeName="fillOpacity"
          begin="100ms" dur="1s"
          values="1;.2;1" calcMode="linear"
          repeatCount="indefinite" />
     </circle>
     <circle cx="52.5" cy="12.5" r="12.5">
-        <animate attributeName="fill-opacity"
+        <animate attributeName="fillOpacity"
          begin="300ms" dur="1s"
          values="1;.2;1" calcMode="linear"
          repeatCount="indefinite" />
     </circle>
     <circle cx="52.5" cy="52.5" r="12.5">
-        <animate attributeName="fill-opacity"
+        <animate attributeName="fillOpacity"
          begin="600ms" dur="1s"
          values="1;.2;1" calcMode="linear"
          repeatCount="indefinite" />
     </circle>
     <circle cx="92.5" cy="12.5" r="12.5">
-        <animate attributeName="fill-opacity"
+        <animate attributeName="fillOpacity"
          begin="800ms" dur="1s"
          values="1;.2;1" calcMode="linear"
          repeatCount="indefinite" />
     </circle>
     <circle cx="92.5" cy="52.5" r="12.5">
-        <animate attributeName="fill-opacity"
+        <animate attributeName="fillOpacity"
          begin="400ms" dur="1s"
          values="1;.2;1" calcMode="linear"
          repeatCount="indefinite" />
     </circle>
     <circle cx="12.5" cy="92.5" r="12.5">
-        <animate attributeName="fill-opacity"
+        <animate attributeName="fillOpacity"
          begin="700ms" dur="1s"
          values="1;.2;1" calcMode="linear"
          repeatCount="indefinite" />
     </circle>
     <circle cx="52.5" cy="92.5" r="12.5">
-        <animate attributeName="fill-opacity"
+        <animate attributeName="fillOpacity"
          begin="500ms" dur="1s"
          values="1;.2;1" calcMode="linear"
          repeatCount="indefinite" />
     </circle>
     <circle cx="92.5" cy="92.5" r="12.5">
-        <animate attributeName="fill-opacity"
+        <animate attributeName="fillOpacity"
          begin="200ms" dur="1s"
          values="1;.2;1" calcMode="linear"
          repeatCount="indefinite" />
