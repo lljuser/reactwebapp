0619cc71a7504c4470f5425565beb7536feb5fc8
jtxu <juntang.xu@sail-fs.com>
2018-02-09 17:37:04 +0800

    update

--- src/abs/views/product/Detail.tsx
+++ src/abs/views/product/Detail.tsx
@@ -12,74 +12,76 @@ class Detail extends React.Component<any, {}> {
     }
 
     return (
-      <table className="appH5_list_two" >
-      <tbody>
-          <tr>
+      <div className="appH5_content">
+        <table className="appH5_list_two" >
+          <tbody>
+            <tr>
               <td>产品名称</td>
               <td>{detail.Basic.DealNameChinese}</td>
-          </tr>
-          <tr>
+            </tr>
+            <tr>
               <td>产品分类</td>
               <td>
-                  <div>
-                    <Link 
-                      to={{ 
+                <div>
+                  <Link
+                    to={{
+                      pathname: `${RoutePageList.HomePage}`,
+                      state: {
+                        type: 'product',
+                        productQuery: {
+                          productTypeValue: [detail.Basic.ProductTypeId]
+                        }
+                      }
+                    }}
+                  >
+                    {detail.Basic.ProductType}
+                  </Link></div>
+                <div>&nbsp;└&nbsp;
+                    <Link
+                      to={{
                         pathname: `${RoutePageList.HomePage}`,
                         state: {
                           type: 'product',
                           productQuery: {
-                            productTypeValue: [detail.Basic.ProductTypeId]  
+                            productTypeValue: [detail.Basic.ProductTypeId],
+                            dealTypeValue: [detail.Basic.DealTypeId]
                           }
-                        } 
+                        }
                       }}
-                    > 
-                    {detail.Basic.ProductType}
-                    </Link></div>
-                  <div>&nbsp;└&nbsp;
-                    <Link 
-                        to={{ 
-                          pathname: `${RoutePageList.HomePage}`,
-                          state: {
-                            type: 'product',
-                            productQuery: {
-                              productTypeValue: [detail.Basic.ProductTypeId],
-                              dealTypeValue: [detail.Basic.DealTypeId] 
-                            }
-                          } 
-                        }}
-                    >  
+                    >
                     {detail.Basic.DealType}
                     </Link></div>
-                  {detail.Basic.AssetSubCategoryId != null ?  <div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;└&nbsp;{detail.Basic.AssetSubCategory}</div> : ''} 
+                {detail.Basic.AssetSubCategoryId != null ? <div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;└&nbsp;{detail.Basic.AssetSubCategory}</div> : ''}
               </td>
-          </tr>
-          <tr>
-          <td>产品状态</td>
-          <td>{detail.Basic.CurrentStatus}</td>
-          </tr>
-          <tr>
-          <td>成立日期</td>
-          <td>
-          {detail.Basic.ClosingDate != null ? moment(detail.Basic.ClosingDate).format('YYYY年MM月DD日') : '-'}</td>
-          </tr>
-          <tr>
-          <td>发起机构</td>
-          <td>
-            {
-              (!detail.Basic || !Array.isArray(detail.Basic.DealOriginator)) ? null : detail.Basic.DealOriginator.map((item) =>
-                <div key={item}>
-                    <span>{item}</span><br/>
-                </div> 
-              )
-            }
-          </td>
-          </tr>
-          <tr>
-          <td style={{verticalAlign: 'middle'}}>金额(亿)</td>
-          <td className="appH5_font_largest appH5_color_red">{detail.Basic.TotalOffering}</td>
-          </tr>
-        </tbody>
-      </table>
+            </tr>
+            <tr>
+              <td>产品状态</td>
+              <td>{detail.Basic.CurrentStatus}</td>
+            </tr>
+            <tr>
+              <td>成立日期</td>
+              <td>
+                {detail.Basic.ClosingDate != null ? moment(detail.Basic.ClosingDate).format('YYYY年MM月DD日') : '-'}</td>
+            </tr>
+            <tr>
+              <td>发起机构</td>
+              <td>
+                {
+                  (!detail.Basic || !Array.isArray(detail.Basic.DealOriginator)) ? '-' : detail.Basic.DealOriginator.map((item) =>
+                    <div key={item}>
+                      <span>{item}</span><br />
+                    </div>
+                  )
+                }
+              </td>
+            </tr>
+            <tr>
+              <td style={{ verticalAlign: 'middle' }}>金额(亿)</td>
+              <td className="appH5_font_largest appH5_color_red">{detail.Basic.TotalOffering}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
